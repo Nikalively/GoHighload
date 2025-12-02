d897ec00d9cb26eb54914d57e716dc516ef61e10
Veronika Bukharina <nika_snow@mail.ru>
2025-12-03 01:28:59 +0500

    Add Dockerfile, docker-compose.yml

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-  "fmt"
+	"gohighload/handlers"
+	"gohighload/metrics"
+	"gohighload/utils"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	r := mux.NewRouter()
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	r.Use(utils.RateLimitMiddleware)
+	r.Use(metrics.MetricsMiddleware)
+
+	r.HandleFunc("/api/users", handlers.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/api/users/{id}", handlers.GetUserHandler).Methods("GET")
+	r.HandleFunc("/api/users", handlers.CreateUserHandler).Methods("POST")
+	r.HandleFunc("/api/users/{id}", handlers.UpdateUserHandler).Methods("PUT")
+	r.HandleFunc("/api/users/{id}", handlers.DeleteUserHandler).Methods("DELETE")
+	r.HandleFunc("/metrics", handlers.MetricsHandler).Methods("GET")
+
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
